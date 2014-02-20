184592b5af0986aa0f3502613e35c9fe400d3563
EsteveCasas <esteve.casas@uvic.cat>
2014-02-20 18:09:29 +0100

    primer test

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by esteve.casas on 20/02/14.
+//  Copyright (c) 2014 JOUIROGER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by esteve.casas on 20/02/14.
+//  Copyright (c) 2014 JOUIROGER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
